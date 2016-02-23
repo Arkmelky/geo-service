6eaf23cf9ab9143eb1b70b827d24430469101efe
Arkady Soloveyko <arkmelky@mail.ru>
2016-02-23 23:15:15 +0300

    Generic Repository Add Signed-off-by: Arkady Soloveyko <arkmelky@mail.ru>

--- Soloveyko_A_V_Geo_Service/Soloveyko_A_V_Geo_Service/Models/DataBase.cs
+++ Soloveyko_A_V_Geo_Service/Soloveyko_A_V_Geo_Service/Models/DataBase.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Data.Entity;
+using System.Data.Entity.Migrations;
 using System.Data.Entity.ModelConfiguration.Conventions;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Web;
 using Microsoft.Ajax.Utilities;
 
@@ -54,5 +56,56 @@ namespace Soloveyko_A_V_Geo_Service.Models
 
         //Location obj
         public DbSet<Location> Locations { get; set; }
+    }
+
+    public class DataBaseRepository<TEntity> where TEntity : class
+    {
+        private DbContext dbContext;
+
+        public DataBaseRepository(DbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+
+        public TEntity Get(int id)
+        {
+            return dbContext.Set<TEntity>().Find(id);
+        }
+
+        public IEnumerable<TEntity> GetAll()
+        {
+            return dbContext.Set<TEntity>().ToList();
+        }
+
+        public IEnumerable<TEntity> Find(Expression<Func<TEntity, bool>> predicate)
+        {
+            return dbContext.Set<TEntity>().Where(predicate);
+        }
+
+        public void Add(TEntity entity)
+        {
+            dbContext.Set<TEntity>().AddOrUpdate(entity);
+        }
+
+        public void AddRange(IEnumerable<TEntity> etities)
+        {
+            foreach (var entity in etities)
+            {
+                dbContext.Set<TEntity>().Add(entity);    
+            }
+        }
+
+        public void Remove(TEntity entity)
+        {
+            dbContext.Set<TEntity>().Remove(entity);
+        }
+
+        public void RemoveRange(IEnumerable<TEntity> entities)
+        {
+            foreach (var entity in entities)
+            {
+                dbContext.Set<TEntity>().Remove(entity);
+            }
+        }
     }
 }
