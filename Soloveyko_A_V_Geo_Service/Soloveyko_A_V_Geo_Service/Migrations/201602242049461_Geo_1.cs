1f11930e1e227057cc3a934d463ffe297a5d5d59
Arkady Soloveyko <arkmelky@mail.ru>
2016-02-24 23:50:53 +0300

    Signed-off-by: Arkady Soloveyko <arkmelky@mail.ru>

---
+++ Soloveyko_A_V_Geo_Service/Soloveyko_A_V_Geo_Service/Migrations/201602242049461_Geo_1.cs
@@ -0,0 +1,16 @@
+namespace Soloveyko_A_V_Geo_Service.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class Geo_1 : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
