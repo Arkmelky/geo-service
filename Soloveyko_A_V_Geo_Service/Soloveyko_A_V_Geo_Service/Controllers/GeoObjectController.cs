ae4afa371af69e4a89ccc584ce6150a6cfaf4f78
Arkady Soloveyko <arkmelky@mail.ru>
2016-02-25 23:27:47 +0300

    CRUD for GeoObject and Location models Signed-off-by: Arkady Soloveyko <arkmelky@mail.ru>

---
+++ Soloveyko_A_V_Geo_Service/Soloveyko_A_V_Geo_Service/Controllers/GeoObjectController.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Soloveyko_A_V_Geo_Service.Models;
+
+namespace Soloveyko_A_V_Geo_Service.Controllers
+{
+    public class GeoObjectController : Controller
+    {
+        private DataBase db = new DataBase();
+
+        //
+        // GET: /GeoObject/
+
+        public ActionResult Index()
+        {
+            var geoobjects = db.GeoObjects.Include(g => g.Location).Include(g => g.Address);
+            return View(geoobjects.ToList());
+        }
+
+        //
+        // GET: /GeoObject/Details/5
+
+        public ActionResult Details(int id = 0)
+        {
+            GeoObject geoobject = db.GeoObjects.Find(id);
+            if (geoobject == null)
+            {
+                return HttpNotFound();
+            }
+            return View(geoobject);
+        }
+
+        //
+        // GET: /GeoObject/Create
+
+        public ActionResult Create()
+        {
+            ViewBag.GeoObjectId = new SelectList(db.Locations, "GeoObjectId", "GeoObjectId");
+            ViewBag.AddressId = new SelectList(db.Addresses, "AddressId", "Country");
+            return View();
+        }
+
+        //
+        // POST: /GeoObject/Create
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(GeoObject geoobject)
+        {
+            if (ModelState.IsValid)
+            {
+                db.GeoObjects.Add(geoobject);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.GeoObjectId = new SelectList(db.Locations, "GeoObjectId", "GeoObjectId", geoobject.GeoObjectId);
+            ViewBag.AddressId = new SelectList(db.Addresses, "AddressId", "Country", geoobject.AddressId);
+            return View(geoobject);
+        }
+
+        //
+        // GET: /GeoObject/Edit/5
+
+        public ActionResult Edit(int id = 0)
+        {
+            GeoObject geoobject = db.GeoObjects.Find(id);
+            if (geoobject == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.GeoObjectId = new SelectList(db.Locations, "GeoObjectId", "GeoObjectId", geoobject.GeoObjectId);
+            ViewBag.AddressId = new SelectList(db.Addresses, "AddressId", "Country", geoobject.AddressId);
+            return View(geoobject);
+        }
+
+        //
+        // POST: /GeoObject/Edit/5
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(GeoObject geoobject)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(geoobject).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.GeoObjectId = new SelectList(db.Locations, "GeoObjectId", "GeoObjectId", geoobject.GeoObjectId);
+            ViewBag.AddressId = new SelectList(db.Addresses, "AddressId", "Country", geoobject.AddressId);
+            return View(geoobject);
+        }
+
+        //
+        // GET: /GeoObject/Delete/5
+
+        public ActionResult Delete(int id = 0)
+        {
+            GeoObject geoobject = db.GeoObjects.Find(id);
+            if (geoobject == null)
+            {
+                return HttpNotFound();
+            }
+            return View(geoobject);
+        }
+
+        //
+        // POST: /GeoObject/Delete/5
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            GeoObject geoobject = db.GeoObjects.Find(id);
+            db.GeoObjects.Remove(geoobject);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            db.Dispose();
+            base.Dispose(disposing);
+        }
+    }
+}
